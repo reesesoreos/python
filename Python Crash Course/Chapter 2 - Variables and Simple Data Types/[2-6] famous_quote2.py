4a47f6d7e437d800de539ddb57bec5d6e6787c0f
reesesoreos <siwoo.j.park@gmail.com>
2026-01-02 13:08:01 -0800

    Added [2-6] famous_quote2.py
    This is a repeat of exercise [2-5], but assigns the message to a variable. Then, the variable is printed.

---
+++ Python Crash Course/Chapter 2 - Variables and Simple Data Types/[2-6] famous_quote2.py
@@ -0,0 +1,4 @@
+famous_person = "albert einstein"
+famous_quote = (f'{famous_person.title()} once said that "A person who has never made a mistake has never tried anything new."')
+
+print(famous_quote)
