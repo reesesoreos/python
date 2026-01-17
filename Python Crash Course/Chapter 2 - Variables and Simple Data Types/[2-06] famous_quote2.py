c43d4a3ea9510e830929c722a7b60a002a40e55a
reesesoreos <siwoo.j.park@gmail.com>
2026-01-17 13:13:30 -0800

    Delete [2-06] famous_quote2.py

--- Python Crash Course/Chapter 2 - Variables and Simple Data Types/[2-06] famous_quote2.py
+++
@@ -1,4 +0,0 @@
-famous_person = "albert einstein"
-famous_quote = (f'{famous_person.title()} once said that "A person who has never made a mistake has never tried anything new."')
-
-print(famous_quote)
