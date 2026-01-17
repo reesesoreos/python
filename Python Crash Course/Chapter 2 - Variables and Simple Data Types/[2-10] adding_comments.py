ecb77728f95e3a774401f4983111c5224d95d4aa
reesesoreos <siwoo.j.park@gmail.com>
2026-01-17 13:16:38 -0800

    Added [2-10] adding_comments.py
    This exercises reuses code, and adds comments to the code.

---
+++ Python Crash Course/Chapter 2 - Variables and Simple Data Types/[2-10] adding_comments.py
@@ -0,0 +1,7 @@
+favorite_number = 30
+print(f"What is my favorite number? My favorite \
+      number is {favorite_number}.")
+
+# This code prints my favorite number.
+# The goal for this exercise is to practice adding comments. 
+# Since I do not have any code at the moment, I am adding this comment to the previous exercise, [2-09].
