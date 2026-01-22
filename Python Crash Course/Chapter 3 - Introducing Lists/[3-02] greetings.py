8f1b7118d64fdb033cf72b0e57d41bb08c20afde
reesesoreos <siwoo.j.park@gmail.com>
2026-01-21 20:41:23 -0800

    Added [3-02] greetings.py
    This stores names in a list. Then, a message is printed using elements from the list.

---
+++ Python Crash Course/Chapter 3 - Introducing Lists/[3-02] greetings.py
@@ -0,0 +1,11 @@
+names = ['john', 'joe', 'bob', 'peter']
+message1 = f"{names[0].title()} is coming with me to the store."
+message2 = f"{names[1].title()} is coming with me to the store."
+message3 = f"{names[2].title()} is coming with me to the store."
+message4 = f"{names[3].title()} is coming with me to the store."
+print(message1)
+print(message2)
+print(message3)
+print(message4)
+
+# At this point in the textbook, loops have not been taught.
