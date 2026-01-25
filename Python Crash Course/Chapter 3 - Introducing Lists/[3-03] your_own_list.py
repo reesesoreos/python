7cd10ea4e287546f80e326724138f2ba3a14f9b5
reesesoreos <siwoo.j.park@gmail.com>
2026-01-24 18:18:15 -0800

    Added [3-03] your_own_list.py
    Compiles modes of transportation into a list. Then, four different statements about each transportation are printed.

---
+++ Python Crash Course/Chapter 3 - Introducing Lists/[3-03] your_own_list.py
@@ -0,0 +1,14 @@
+transportation = ['bus', 'motorcycle', 'bike', 'car']
+message1 = f"I do not ride a {transportation[0]} very often."
+print(message1)
+
+message2 = f"I have never ridden a {transportation[1]} before."
+print(message2)
+
+message3 = f"I like to ride a {transportation[2]} on the weekends."
+print(message3)
+
+message4 = f"I ride in a {transportation[3]} every day."
+print(message4)
+
+# At this point in the textbook, loops have not been taught.
