6d42c0b08fff9d467ff47490f0db402e0e45a8b6
reesesoreos <siwoo.j.park@gmail.com>
2026-02-28 18:43:32 -0800

    Added [3-04] guest_list.py
    This makes a list of people to invite to dinner, then prints messages inviting these people to dinner.

---
+++ Python Crash Course/Chapter 3 - Introducing Lists/[3-04] guest_list.py
@@ -0,0 +1,4 @@
+dinner_list = ['george washington', 'benjamin franklin', 'abraham lincoln']
+print(f"Hello, {dinner_list[0].title()}, I would like to invite you to dinner.")
+print(f"Hello, {dinner_list[1].title()}, I would like to invite you to dinner.")
+print(f"Hello, {dinner_list[2].title()}, I would like to invite you to dinner.")
