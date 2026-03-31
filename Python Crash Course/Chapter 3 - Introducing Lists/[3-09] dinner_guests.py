be828eae21f1fc98519fbb95f5c6b7359fb292a0
reesesoreos <siwoo.j.park@gmail.com>
2026-03-30 17:44:50 -0700

    Added [3-09] dinner_guests.py
    Reuses exercise [3-04]. Uses the function len.

---
+++ Python Crash Course/Chapter 3 - Introducing Lists/[3-09] dinner_guests.py
@@ -0,0 +1,6 @@
+dinner_list = ['george washington', 'benjamin franklin', 'abraham lincoln']
+print(f"Hello, {dinner_list[0].title()}, I would like to invite you to dinner.")
+print(f"Hello, {dinner_list[1].title()}, I would like to invite you to dinner.")
+print(f"Hello, {dinner_list[2].title()}, I would like to invite you to dinner.")
+
+print(f"I am inviting {len(dinner_list)} people to dinner tonight.")
