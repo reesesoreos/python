e2679f329a9b240a50e4075a1179bda3eb69ece4
reesesoreos <siwoo.j.park@gmail.com>
2026-04-05 18:14:35 -0700

    Added [3-11] intentional_error.py
    This makes an intentional index error, then fixes it.

---
+++ Python Crash Course/Chapter 3 - Introducing Lists/[3-11] intentional_error.py
@@ -0,0 +1,4 @@
+subjects = ['math', 'science', 'english']
+#print(subjects[3]) would be the original function, but this would cause a "list index out of range" error.
+#The new function gets the last item in the list with no error.
+print(subjects[-1])
