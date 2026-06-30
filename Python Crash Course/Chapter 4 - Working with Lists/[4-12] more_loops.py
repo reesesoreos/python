54ced18d007573c9d925a07a447e53d24ab0760f
reesesoreos <siwoo.j.park@gmail.com>
2026-06-30 08:43:51 -0700

    Added [4-12] more_loops.py
    This reuses the examples from the book and uses some loops.

---
+++ Python Crash Course/Chapter 4 - Working with Lists/[4-12] more_loops.py
@@ -0,0 +1,14 @@
+#This reuses the foods.py examples shown in the book.
+my_foods = ['pizza', 'falafel', 'carrot cake']
+friend_foods = my_foods[:]
+
+my_foods.append('cannoli')
+friend_foods.append('ice cream')
+
+print("My favorite foods are:")
+for food in my_foods:
+    print(f"{food.title()}")
+
+print("My friend's favorite foods are:")
+for food in friend_foods:
+    print(f"{food.title()}")
