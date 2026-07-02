85a8d92ac7980333526343728f52c7636d61fb1f
reesesoreos <siwoo.j.park@gmail.com>
2026-07-01 23:08:21 -0700

    Added [4-13] buffet.py
    This uses tuples for the first time and uses some functions with it.

---
+++ Python Crash Course/Chapter 4 - Working with Lists/[4-13] buffet.py
@@ -0,0 +1,11 @@
+buffet_foods = ('fried rice', 'chicken', 'steak', 'sushi', 'potatoes')
+for food in buffet_foods:
+    print(f"The buffet offers {food}.")
+
+print("\n")
+
+#Intentional function that would result in an error: buffet_foods[0] = ("rice"). This was asked for in the exercise.
+
+buffet_foods = ('orange chicken', 'tacos', 'steak', 'sushi', 'potatoes')
+for food in buffet_foods:
+    print(f"The buffet offers {food}.")
