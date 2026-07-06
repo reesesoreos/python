692e9737cd4eebbee69cddda629990cf30e9e66d
reesesoreos <siwoo.j.park@gmail.com>
2026-07-06 10:48:51 -0700

    Added [5-01] conditional_tests.py
    This creates 10 conditional tests and predicts the outcome.

---
+++ Python Crash Course/Chapter 5 - If Statements/[5-01] conditional_tests.py
@@ -0,0 +1,37 @@
+car = 'subaru'
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+print("Is car == 'audi'? I predict False.")
+print(car == 'audi')
+
+print()
+
+computer = 'macbook'
+print("Is computer == 'macbook'? I predict True.")
+print(computer == 'macbook')
+print("Is computer == 'chromebook'? I predict False.")
+print(computer == 'chromebook')
+
+print()
+
+board_game = 'chess'
+print("Is board_game == 'chess'? I predict True.")
+print(board_game == 'chess')
+print("Is board_game == 'checkers'? I predict False.")
+print(board_game == 'checkers')
+
+print()
+
+car = 'bmw'
+print("Is car == 'bmw'? I predict True.")
+print(car == 'bmw')
+print("Is car == 'tesla'? I predict False.")
+print(car == 'tesla')
+
+print()
+
+clothing_brand = 'nike'
+print("Is clothing_brand == 'nike'? I predict True.")
+print(clothing_brand == 'nike')
+print("Is clothing_brand == 'adidas'? I predict False.")
+print(clothing_brand == 'adidas')
