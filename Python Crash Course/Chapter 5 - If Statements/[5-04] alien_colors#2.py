56626eb61c73f6f516d9c56549996169d4825fa9
reesesoreos <siwoo.j.park@gmail.com>
2026-07-09 08:57:35 -0700

    Added [5-04] alien_colors#2.py
    This gives a certain amount of points based on the alien's color.

---
+++ Python Crash Course/Chapter 5 - If Statements/[5-04] alien_colors#2.py
@@ -0,0 +1,15 @@
+alien_color = 'green'
+if alien_color == 'green':
+    point = 5
+else:
+    point = 10
+
+print(f"You have earned {point} points.")
+
+alien_color = 'red'
+if alien_color == 'green':
+    point = 5
+else:
+    point = 10
+
+print(f"You have earned {point} points.")
