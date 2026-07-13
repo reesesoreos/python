8da86ba3710eca6db6ab8aa1fba30ec66ccc5465
reesesoreos <siwoo.j.park@gmail.com>
2026-07-13 11:16:15 -0700

    Added [5-07] favorite_fruit.py
    This uses a series of if statements to print messages based on the item.

---
+++ Python Crash Course/Chapter 5 - If Statements/[5-07] favorite_fruit.py
@@ -0,0 +1,16 @@
+favorite_fruits = ['watermelon', 'strawberry', 'grape']
+
+if 'watermelon' in favorite_fruits:
+    print("You really like watermelon!")
+
+if 'banana' in favorite_fruits:
+    print("You really like banana!")
+
+if 'grape' in favorite_fruits:
+    print("You really like grape!")
+
+if 'orange' in favorite_fruits:
+    print("You really like orange!")
+    
+if 'strawberry' in favorite_fruits:
+    print("You really like strawberry!")
