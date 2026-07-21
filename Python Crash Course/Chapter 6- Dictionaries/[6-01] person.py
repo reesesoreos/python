9aa3991ce1bfbc1dc8c44ff790fcd5db3dba0e50
reesesoreos <siwoo.j.park@gmail.com>
2026-07-21 15:02:49 -0700

    Added [6-01] person.py
    This defines and then prints information in a dictionary.

---
+++ Python Crash Course/Chapter 6- Dictionaries/[6-01] person.py
@@ -0,0 +1,5 @@
+person_information = {'first_name' : 'brenden', 'last_name' : 'park', 'age' : 17, 'city' : 'los angeles'}
+print(person_information['first_name'])
+print(person_information['last_name'])
+print(person_information['age'])
+print(person_information['city'])
