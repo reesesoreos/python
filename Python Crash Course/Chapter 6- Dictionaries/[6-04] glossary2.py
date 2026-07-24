4b33f0cddbaea0f3d7a8ea8ac1f90826e62d1ee0
reesesoreos <siwoo.j.park@gmail.com>
2026-07-24 16:59:59 -0700

    Added [6-04] glossary2.py
    This extends the glossary and loops through each key and value.

---
+++ Python Crash Course/Chapter 6- Dictionaries/[6-04] glossary2.py
@@ -0,0 +1,17 @@
+glossary = {
+    'list' : 'collection of items in a variable',
+    'for loop' : 'a loop that repeats a block of code',
+    'if statement' : 'a statement that evaluates a condition',
+    'if-else statement' : 'statement that runs certain code based on its '
+    'conditional value', 
+    'if-elif-else chain' : 'chain that checks multiple statements',
+    'dictionary' : 'a collection of key-value pairs',
+    'string': 'a series of characters',
+    'variable': 'a label that holds a value',
+    'boolean': 'a value that is either True or False',
+    'comment': 'a note in the code ignored by Python'}
+
+for key, definition in glossary.items():
+    print(key)
+    print(definition)
+    print()
