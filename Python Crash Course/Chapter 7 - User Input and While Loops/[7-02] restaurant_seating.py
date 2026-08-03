38a67eea5eb7b95133b91a054e2e07fc6093133d
reesesoreos <siwoo.j.park@gmail.com>
2026-08-03 09:44:20 -0700

    Added [7-02] restaurant_seating.py
    This uses an if-else statement to print messages based on a user's input.

---
+++ Python Crash Course/Chapter 7 - User Input and While Loops/[7-02] restaurant_seating.py
@@ -0,0 +1,6 @@
+guest_number = int(input("How many people are in your party today? "))
+
+if guest_number > 8:
+    print("You will have to wait for a table.")
+else: 
+    print("Your table is ready.")
