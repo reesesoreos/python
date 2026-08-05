541648b0a0151eeaf9f4bb2bf4c093eca2aad0e7
reesesoreos <siwoo.j.park@gmail.com>
2026-08-05 13:07:59 -0700

    Added [7-04] pizza_toppings.py
    This uses while loops with inputs to print certain messages.

---
+++ Python Crash Course/Chapter 7 - User Input and While Loops/[7-04] pizza_toppings.py
@@ -0,0 +1,6 @@
+while True:
+    topping = (input("Please choose a topping to put on your pizza. "))
+    if topping == 'quit':
+        break
+    else:
+        print(f"I will add {topping} to your pizza.")
