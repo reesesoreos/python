11587890e63bb91bec10edf1ccc28347146e9d80
reesesoreos <siwoo.j.park@gmail.com>
2026-08-15 10:11:49 -0700

    Added [8-04] large_shirts.py
    This calls a function that already has default arguments.

---
+++ Python Crash Course/Chapter 8 - Functions/[8-04] large_shirts.py
@@ -0,0 +1,6 @@
+def make_shirt(size='large', text='I love Python'):
+    print(f"You ordered a {size} shirt, with {text} on it.")
+
+make_shirt()
+make_shirt(size='medium')
+make_shirt(size='small', text='Nike')
