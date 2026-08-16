2affb496238dc30d71c9a03c69192e36c9732791
reesesoreos <siwoo.j.park@gmail.com>
2026-08-16 13:48:35 -0700

    Added [8-05] cities.py
    This defines a function and calls it multiple times, using different arguments each time.

---
+++ Python Crash Course/Chapter 8 - Functions/[8-05] cities.py
@@ -0,0 +1,6 @@
+def describe_city(city, country='France'):
+    print(f"{city.title()} is in {country.title()}.")
+
+describe_city('paris')
+describe_city('marseille')
+describe_city('london', 'england')
