7a286ae44ad589f06702e76f1d4b58f9a24fd534
reesesoreos <siwoo.j.park@gmail.com>
2026-08-19 17:54:22 -0700

    Added [8-09] messages.py
    This calls a function that loops through a list.

---
+++ Python Crash Course/Chapter 8 - Functions/[8-09] messages.py
@@ -0,0 +1,12 @@
+text_messages = [
+    'ok',
+    'yes',
+    'no',
+    'hello',
+]
+
+def show_messages(text_messages):
+    for msg in text_messages:
+        print(msg)
+
+show_messages(text_messages)
