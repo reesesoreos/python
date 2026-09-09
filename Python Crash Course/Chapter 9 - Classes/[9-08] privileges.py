1158877c3b6c091b789c939f870d3557cbae81e6
reesesoreos <siwoo.j.park@gmail.com>
2026-09-08 21:46:13 -0700

    Added [9-08] privileges.py
    This calls methods within a composition of classes.

---
+++ Python Crash Course/Chapter 9 - Classes/[9-08] privileges.py
@@ -0,0 +1,36 @@
+# Reuses exercise [9-07]
+class User:
+    def __init__(
+            self, first_name, last_name, language_preference, display_name
+            ):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.language_preference = language_preference
+        self.display_name = display_name
+
+    def describe_user(self):
+        print(f"This user's first name is {self.first_name.title()}.")
+        print(f"This user's last name is {self.last_name.title()}.")
+        print(f"This user's language preference is {self.language_preference.title()}.")
+        print(f"This user's display name is {self.display_name}.")
+    
+    def greet_user(self):
+        print(f"Hello {self.first_name.title()}, welcome to the platform.")
+
+class Admin(User):
+    def __init__(self, first_name, last_name, language_preference, display_name):
+        super().__init__(first_name, last_name, language_preference, display_name)
+        self.privileges = Privileges()
+    
+
+class Privileges:
+    def __init__(self):
+        self.privileges = ['delete users', 'ban users', 'delete posts']
+
+    def show_privileges(self):
+        for privilege in self.privileges:
+            print(f"The admin can {privilege}.")
+
+
+admin = Admin('alex', 'smith', 'english', 'AlexS',)
+admin.privileges.show_privileges()
