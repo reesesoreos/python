3d61e36191d4e367f0bfcd86e62bda3b3644b347
reesesoreos <siwoo.j.park@gmail.com>
2026-09-10 23:09:09 -0700

    Added restaurantimport.py
    This file defines a class to be imported into other files.

---
+++ Python Crash Course/Chapter 9 - Classes/modules/restaurantimport.py
@@ -0,0 +1,15 @@
+
+
+class Restaurant:
+    def __init__(self, restaurant_name, cuisine_type):
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+    
+    def describe_restaurant(self):
+        print(f"The restaurant's name is {self.restaurant_name.title()}.")
+        print(f"The cuisine type of the restaurant is {self.cuisine_type.title()}.")
+    
+    def open_restaurant(self):
+        print(f"{self.restaurant_name.title()} is open.")
+    
+restaurant = Restaurant('panda express', 'chinese')
